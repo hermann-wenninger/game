f47553ab6b21fa3bc3168b86145ab08d2f4243b0
hermann <hermann23@vivaldi.net>
2021-11-24 19:41:07 +0100

    l

--- src/app/core/level.js
+++ src/app/core/level.js
@@ -2,11 +2,18 @@ const LEVEL_WIDTH = 13;
 const LEVEL_HEIGHT = 13;
 
 class Level {
+    #columns = [];
+    render (){}
+
+    arnBomb(){}
+
+    canMove(){}
+
+    destroy(){}
 
 
 
 
 
 
-    
 }
