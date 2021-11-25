f5be5a4e13a7cc6e5312cc8c61af99f00230d33b
hermann <hermann23@vivaldi.net>
2021-11-25 17:32:48 +0100

    createlevelitem and createcellgroup in level.js

--- src/app/core/level.js
+++ src/app/core/level.js
@@ -3,14 +3,19 @@ const LEVEL_HEIGHT = 13;
 
 class Level {
   #columns = [];
+  #levelElement = document.createElement("div");
+  #rootElement = document.getElementById("root");
 
   render() {
-      const createLevelItem = () => {return new LevelItem();};
-      const createCellGroup = (column) => {
+    const createLevelItem = () => {
+      return new LevelItem();
+    };
+    const createCellGroup = (column) => {
       return new Array(LEVEL_HEIGHT).fill(null).map(createLevelItem);
     };
     this.#columns = new Array(LEVEL_WIDTH).fill(null).map(createCellGroup);
-    debugger;
+    this.#levelElement.classList.add("level");
+    this.#rootElement.appendChild(this.#levelElement);
   }
 
   armBomb() {}
