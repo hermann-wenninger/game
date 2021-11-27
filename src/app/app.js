fd2cf5d310cce4332eaaa7b491001ae3f0d518ff
hermann <hermann23@vivaldi.net>
2021-11-27 11:37:20 +0100

    minimal thinking maximal following

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,6 @@
 import { Level } from './core/level';
 import { LevelItem } from './core/level-item';
+import { levelItemType } from '.core/constants';
 window.addEventListener('DOMContentLoaded', () =>{
 const level = new Level();
 level.render();
--- src/app/core/level.js
+++ src/app/core/level.js
@@ -1,6 +1,6 @@
 import "./level.scss";
-import { levelItemType } from './constants';
-import { levelItem } from './level-item';
+import { levelItemType } from "./constants";
+import { levelItem } from "./level-item";
 
 const LEVEL_WIDTH = 13;
 const LEVEL_HEIGHT = 13;
@@ -18,27 +18,33 @@ export class Level {
       return new Array(LEVEL_HEIGHT).fill(null).map(createLevelItem);
     };
     this.#columns = new Array(LEVEL_WIDTH).fill(null).map(createCellGroup);
-   
+
     this.#initializeWalls();
-    
-    this.#columns.forEach((column, columnIndex)=>{
-      const columnElement = document.createElement('div');
 
-      column.forEach((cell,cellIndex)=>{
-        const cellElement = document.createElement('div');
-        cellElement.classList.add('cell');
+    this.#columns.forEach((column, columnIndex) => {
+      const columnElement = document.createElement("div");
 
-        switch(cell.type){
-          case levelItemType.OUTER_WALL:{
-            cellElement.classList.add('outer-wall');
+      column.forEach((cell, cellIndex) => {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("cell");
+
+        switch (cell.type) {
+          case levelItemType.OUTER_WALL: {
+            cellElement.classList.add("outer-wall");
+            break;
+          }
+          case levelItemType.HARD_WALL:{
+            cellElement.classList.add('hard-wall');
             break;
           }
         }
         columnElement.appendChild(cellElement);
-
       });
-    })
+      this.#levelElement.appendChild(columnElement);
+    });
     this.#levelElement.classList.add("level");
+    //this.#levelElement.appendChild(columnElement);
+
     this.#rootElement.appendChild(this.#levelElement);
   }
 
@@ -48,17 +54,24 @@ export class Level {
 
   destroy() {}
 
-  #initializeWalls(){
-    this.#columns.forEach((column, columnIndex)=>{
+  #initializeWalls() {
+    this.#columns.forEach((column, columnIndex) => {
       const isFirstColumn = columnIndex === 0;
-      const isLastColumn = columnIndex === LEVEL_WIDTH-1;
-      column.forEach((cell,cellIndex)=>{
-        if(isFirstColumn|| isLastColumn){
+      const isLastColumn = columnIndex === LEVEL_WIDTH - 1;
+      column.forEach((cell, cellIndex) => {
+        if(cellIndex===6&&columnIndex===6){
+          
+        }
+        const isFirstRow = cellIndex === 0;
+        const isLastRow = cellIndex === LEVEL_HEIGHT - 1;
+        const isHardWall = columnIndex % 2 === 0 && cellIndex % 2 === 0;
+        if (isFirstColumn || isLastColumn || isFirstRow || isLastRow) {
           cell.type = levelItemType.OUTER_WALL;
+        } else if (isHardWall === true) {
+          cell.type = levelItemType.HARD_WALL;
         }
       });
-
     });
-    console.log(this.#columns)
+    console.log(this.#columns);
   }
 }
